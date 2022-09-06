02d7139b95031247436f89244cb93625367424f6
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-09-06 16:02:38 +0200

    create array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,43 @@
 console.log("Martedì");
+
+/*
+Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+Scott Estrada	Developer	scott-estrada-developer.jpg
+Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+*/
+
+const team = [
+  (player1 = {
+    nome: "Wayne",
+    ruolo: "Founder & CEO",
+    foto: "./img/wayne-barnett-founder-ceo.jpg",
+  }),
+  (player2 = {
+    nome: "Angela",
+    ruolo: "Chief Editor",
+    foto: "./img/angela-caroll-chief-editor.jpg",
+  }),
+  (player3 = {
+    nome: "Walter",
+    ruolo: "Office Manager",
+    foto: "./img/walter-gordon-office-manager.jpg",
+  }),
+  (player4 = {
+    nome: "Angela",
+    ruolo: "Social Media Manager",
+    foto: "./img/angela-lopez-social-media-manager.jpg",
+  }),
+  (player5 = {
+    nome: "Scott",
+    ruolo: "Developer",
+    foto: "./img/scott-estrada-developer.jpg",
+  }),
+  (player6 = {
+    nome: "Barbare",
+    ruolo: "Graphic Designer",
+    foto: "./img/barbara-ramos-graphic-designer.jpg",
+  }),
+];
